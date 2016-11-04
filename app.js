b9cb1e3af06ff56baac2b076da26e6061d979860
Boasbabs <babalolasimeon@gmail.com>
2016-11-04 05:43:16 +0100

    Add nested views in home by creating 2 states to <a> tag

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ routerApp.config(["$stateProvider", "$urlRouterProvider", function($stateProvide
       url: "/home",
       templateUrl: "partial-home.html"
     })
-    .state("about",{
-
+    .state("home.list",{
+      url: "/list",
+      templateUrl: "partial-home-list.html",
+      controller: function($scope) {
+        $scope.dogs = ["Dingo", "Husky", "Bulldog"];
+      }
+    })
+    .state("home.paragraph", {
+      url: "/paragraph",
+      template: "<h2>I could use a drink right now.</h2>"
     });
 }])
