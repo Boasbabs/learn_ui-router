0e3fe26bf3eb2f3e794dc5d1968e098d8e81748f
Boasbabs <babalolasimeon@gmail.com>
2016-11-04 10:46:32 +0100

    Create form files, form routes with formController

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var routerApp = angular.module("routerApp", ["ngAnimate", "ui.router" ]);
 
 routerApp.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
 
+  // ROUTE ===========================
   $urlRouterProvider.otherwise("/home");
-
     // HOME STATES AND NESTED VIEWS==============
   $stateProvider
     .state("home", {
@@ -36,8 +36,37 @@ routerApp.config(["$stateProvider", "$urlRouterProvider", function($stateProvide
         }
       }
     });
+}])
+// THE ROUTE FOR THE FORM PAYMENT
+routerApp.config(["$urlRouterProvider","$stateProvider", function($urlRouterProvider, $stateProvider) {
+
+  // ROUTE FOR THE basic FORM
+  $stateProvider
+    .state("form",{
+      url: "/form",
+      templateUrl: "form.html",
+      controller: "formController"
+    })
+    // nested states
+    // each of these sections will have their own view
+    // url will be nested (/form/profile)
+    .state("form.profile",{
+      url: "/profile",
+      templateUrl: "form-profile.html"
+    })
+    // url will be /form/interests
+    .state("form.interests",{
+      url: "/interests",
+      templateUrl: "form-interests.html"
+    })
+    // url will be /form/payment
+    .state("form.payment",{
+      url: "/payment",
+      templateUrl: "form-payment.html"
+    });
 }]);
 
+// CONTROLLERS ===============================
 // the scotchController in about state
 routerApp.controller("scotchController", ["$scope", function($scope) {
   $scope.message = "test";
@@ -56,4 +85,16 @@ routerApp.controller("scotchController", ["$scope", function($scope) {
         price: 20000
     }
   ];
+}]);
+
+routerApp.controller("formController",["$scope", function($scope) {
+
+  // an object to store data
+  $scope.formData = {
+
+  };
+  // function to process the form
+  $scope.processForm = function() {
+    alert("Form completed!");
+  }
 }]);
