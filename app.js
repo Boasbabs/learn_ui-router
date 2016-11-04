0c840be890f4efb05c5e82c80b11663fd4db9c9c
Boasbabs <babalolasimeon@gmail.com>
2016-11-04 10:15:42 +0100

    Add 2 views to the about page & create controller for columnTwo

--- app.js
+++ app.js
@@ -20,5 +20,40 @@ routerApp.config(["$stateProvider", "$urlRouterProvider", function($stateProvide
     .state("home.paragraph", {
       url: "/paragraph",
       template: "<h2>I could use a drink right now.</h2>"
+    })
+    .state("about", {
+      url: "/about",
+      views: {
+        // the main template will be placed here (relatively named)
+        "":{templateUrl: "partial-about.html"},
+        // the child views will be defined here (absolutely named)
+        "columnOne@about": {template: "Look I am a column!"},
+
+        // for column two, we'll define a separate controller
+        "columnTwo@about": {
+          templateUrl: "table-data.html",
+          controller: "scotchController"
+        }
+      }
     });
-}])
+}]);
+
+// the scotchController in about state
+routerApp.controller("scotchController", ["$scope", function($scope) {
+  $scope.message = "test";
+
+  $scope.scotches = [
+    {
+      name: "Macmillan 16",
+      price: 5000
+    },
+    {
+        name: 'Chivas Regal Royal Salute',
+        price: 10000
+    },
+    {
+        name: 'Glenfiddich 1937',
+        price: 20000
+    }
+  ];
+}]);
